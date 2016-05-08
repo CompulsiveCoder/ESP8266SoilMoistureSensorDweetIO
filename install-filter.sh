f33e7cb129a1f585d352dd46ce79b9cb46f6d6fb
CompulsiveCoder <compulsivecoder@gmail.com>
2016-05-08 15:26:49 +1000

    Updated install filter script

--- install-filter.sh
+++ install-filter.sh
@@ -1,5 +1,9 @@
+# Remove WiFi SSID and password
+#git config filter.password.clean "sed -e 's/[RealPassword]/[PasswordPlaceHolder]/' -e 's/[RealSSID]/[SSIDPlaceHolder]/'"
 git config filter.password.clean "sed -e 's/-PASSWORD-/-PASSWORD-/' -e 's/-SSID-/-SSID-/'"
 
+# Restore WiFi SSID and password
+#git config filter.password.smudge "sed -e 's/[PasswordPlaceHolder]/[RealPassword]/' -e 's/[SSIDPlaceHolder]/[RealSSID]/'"
 git config filter.password.smudge "sed -e 's/-PASSWORD-/-PASSWORD-/' -e 's/-SSID-/-SSID-/'"
 
 cat > .git/info/attributes << EOF
